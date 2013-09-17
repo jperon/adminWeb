362124b59bab56099eace6ca11733b165cd71ab1
root <root@routeur>
2013-09-17 17:33:20 +0200

    ssl − séparation de la configuration

--- adminweb.py
+++ adminweb.py
@@ -1,123 +1,14 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 import string,time,subprocess
-import os,sys,cgi,base64,socket
+import os,sys,cgi,base64,socket,ssl
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+from config import *
 
 #Program developed to control linux in a web fashion
 #AUThOR : ROBERTO RIBEIRO - PRODASEN SSA
 #DATE : 31/05/2011
-
-#START  CONFIGURATION
-#General Service Configuration - ip (use 0.0.0.0 and keep it generic), port , hostname(do not use,but keep the empty string) , user , password, head of the pages,label of the button that brings the main screen, label of the save button,label of the confirmation
-baseConfig = ['0.0.0.0','10500','','admin','Pardal','PRODASEN - SSA -','Main Panel','Save','Are you sure?']
-#Per administered service configuration
-#You must add an array named Config that has N child arrays for each set of buttons in the following format:
-#First String with then name of the administered service 
-# N Buttons of that administered service. It can be of the following types (5):
-
-# Type 1 - "Call with screen" format (used in scripts of /etc/init.d to start/stop/reload services):
- #Label
- #Button type : 1
- #Array of command and parameters
- #1 - Has confirmation prompt 0 - do not has confirmation prompt
- #Message to present in the end of processing (no matter what happened, this message wil be shown)
-
-#Type 2 - "System with Screen" format (used to execute scripts - like status - and capture the result):
-#DANGER: DO NOT USE THIS BUTTON TO START SCRIPTS THAT WILL LAUNCH PROCESSES IN BACKGROUD(like /etc/init.d scripts) - USE TYPE 1 IN THIS CASE 
-#DANGER: USE THIS TYPE TO LAUNCH SCRIPTS THAT WILL END WITHOUT going BACKGROUND 
- #Label 
- #Button type : 2
- #String with the command (can have pipes)
- #1 - Has confirmation prompt 0 - do not has confirmation prompt
- #return menssage when success
- #return message when fails
-
-#Type 3 - "File Edit" format
- #Label 
- #Button type : 3
- #String with the file to be edited
- #1 - Has confirmation prompt 0 - do not has confirmation prompt
- 
-#Type 4 - "Watch result file" format (Used to run commands  that will show results - like a processing with awk  ):
- #Label 
- #Button type  : 4
- #String with the commands that will have the result piped to a known file that will be shown 
- #1 - Has confirmation prompt 0 - do not has confirmation prompt
- 
-
-#Type 5 - "File upload" format:
- #Label 
- #Button type : 5
- #Upload directory (like "/opt/test/" dont forget the bars)
- #1 - Has confirmation prompt 0 - do not has confirmation prompt
- #Message in the screen that chooses the file 
- #Size of the label that shows the file name
- #Menssage in case of success
-
-
-config = [
-         
-           [
-            'SQUID',
-            ['Stop','1',['/etc/init.d/squid3','stop'],'1','Check the log after this operation.'],
-            ['Start','1',['/etc/init.d/squid3','start'],'1','Check the log after this operation.'],
-            ['Status','2','ps -ef | grep -i squid3| grep -vi grep','0','Squid ON','Squid OFF'],
-            ['Reload','1',['/usr/sbin/squid3','-k','reconfigure'],'1','Check the log after this operation.'],
-            ['Configuration','3','/etc/squid3/squid.conf','0'],
-            ['Access Log','4','tail -500 /var/log/squid3/access.log','0'],
-            ['Error Log','4','tail -500 /var/log/squid3/cache.log','0'],
-           ],
-
-           [
-            'POUND',
-            ['Stop','1',['/etc/init.d/pound','stop'],'1','Check the log after this operation.'],
-            ['Start','1',['/etc/init.d/pound','start'],'1','Check the log after this operation.'],
-            ['Status','2','ps -ef | grep -i pound| grep -vi grep','0','Pound ON','Pound OFF'],
-            ['Configuration','3','/etc/pound/pound.cfg','0'],
-            ['Error Log','4','tail -500 /var/log/syslog','0'],
-           # ['Upload','5','/home/webadmin/','0','File to upload','40','File Upload OK'],
-           ],
-
-           [
-            'HEARTBEAT',
-            ['Stop','1',['/etc/init.d/heartbeat','stop'],'1','Check the log after this operation.'],
-            ['Start','1',['/etc/init.d/heartbeat','start'],'1','Check the log after this operation.'],
-            ['Status','2','ps -ef | grep -i heartbeat| grep -vi grep','0','Heartbeat ON','Heartbeat OFF'],
-            ['Reload','1',['/etc/init.d/heartbeat','reload'],'1','Check the log after this operation.'],
-            ['Configuration','3','/etc/ha.d/ha.cf','0'],
-            ['Error Log','4','tail -500 /var/log/ha-log','0'],
-            ['Active/Passive','2','ifconfig|grep -i 172\.31\.0\.24','0','Host status : ACTIVE','Host status: Passive'],
-            ['Set Active','1',['/usr/lib/heartbeat/hb_takeover'],'1','Check the log after this operation.'],
-            ['Set Passive','1',['/usr/lib/heartbeat/hb_standby'],'1','Check the log after this operation.'],
-           ],
-
-
-
-         ]
-#END - CONFIGURATION
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Initial point of the program - DO NOT CHANGE ANYTHING FROM THIS POINT
 bstyle="style='color:white;background-color:#55575c;font-color:white;width:120;height:35;border:1px solid;'"
 hostname = socket.gethostname()
@@ -130,15 +21,15 @@ class Handler(BaseHTTPRequestHandler):
            if self.headers.getheader('Authorization') == None or self.headers.getheader('Authorization') != 'Basic '+basic:
               self.send_response(401)
               self.send_header('WWW-Authenticate', 'Basic realm=\"adminWeb\"')
-              self.send_header('Content-type', 'text/html')
+              self.send_header('Content-type', 'text/html; charset=utf-8')
               self.end_headers()
-              self.wfile.write("<html><head></head><body style='color:white;background-color:grey'><center>"+baseConfig[5]+" AdminWeb 1.0</center>") 
+              self.wfile.write("<html><head></head><body style='color:white;background-color:grey'><center>"+baseConfig[5]+" </center>") 
               self.wfile.write("<center><br><br>Acesso nao autorizado</center></body></html>") 
               return
            self.send_response(200)
-           self.send_header('Content-type','text/html') 
+           self.send_header('Content-type','text/html; charset=utf-8') 
            self.end_headers()
-           self.wfile.write("<html><head></head><body style='color:white;background-color:grey'><center>"+baseConfig[5]+" AdminWeb 1.0</center><br><center>Server : "+hostname+"</center>") 
+           self.wfile.write("<html><head></head><body style='color:white;background-color:grey'><center>"+baseConfig[5]+" </center><br><center>"+hostname+"</center>") 
            if self.path.endswith("editaarquivo") or  self.path.endswith("executaTarefa") or self.path.endswith("logtail")or self.path.endswith("uploadprepara"):
              linha=self.path[1]
              coluna=self.path[2]
@@ -146,18 +37,18 @@ class Handler(BaseHTTPRequestHandler):
            if self.path.endswith("editaarquivo"):
              temp = config[int(linha)][int(coluna)]
              f = open(temp[2],"rb") 
-             self.wfile.write("<center><form name='arquivoedit' method='POST' action='http://"+hostname+":"+baseConfig[1]+"/"+linha+coluna+"salvaarquivo'><textarea name='texto' COLS=145 ROWS=20>"+f.read()+"</textarea><br><br><button "+bstyle+" onclick=\"javascript:if (confirm('"+baseConfig[8]+"')){document.arquivoupload.submit()};return false;\">"+baseConfig[7]+"</button></form><button "+bstyle+" onclick='javascript:window.document.location=\"http://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center>")
+             self.wfile.write("<center><form name='arquivoedit' method='POST' action='https://"+hostname+":"+baseConfig[1]+"/"+linha+coluna+"salvaarquivo'><textarea name='texto' COLS=145 ROWS=20>"+f.read()+"</textarea><br><br><button "+bstyle+" onclick=\"javascript:if (confirm('"+baseConfig[8]+"')){document.arquivoupload.submit()};return false;\">"+baseConfig[7]+"</button></form><button "+bstyle+" onclick='javascript:window.document.location=\"https://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center>")
              f.close()
 
            elif self.path.endswith("logtail"):
              temp = config[int(linha)][int(coluna)]
              os.system(temp[2]+" >"+caminho+"/logtail.log")             
              f = open(caminho+"/logtail.log","r")
-             self.wfile.write("<center><form name='xxx' method='POST' action=''><textarea name='texto' COLS=145 ROWS=20>"+f.read()+"</textarea><br><br></form><button "+bstyle+" onclick='javascript:window.document.location=\"http://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center>")
+             self.wfile.write("<center><form name='xxx' method='POST' action=''><textarea name='texto' COLS=145 ROWS=20>"+f.read()+"</textarea><br><br></form><button "+bstyle+" onclick='javascript:window.document.location=\"https://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center>")
              f.close()
            elif self.path.endswith("uploadprepara"):
              temp = config[int(linha)][int(coluna)]
-             self.wfile.write("<center><form name='arquivoupload' method='POST' enctype=\"multipart/form-data\" action='http://"+hostname+":"+baseConfig[1]+"/"+linha+coluna+"uploadarquivo'><br>"+temp[4]+"<br><input type=\"file\" name=\"file\" size=\""+temp[5]+"\"><br><br><button "+bstyle+" onclick=\"javascript:if (confirm('"+baseConfig[8]+"')){document.arquivoupload.submit()};return false;\">Upload</button></form><button "+bstyle+" onclick='javascript:window.document.location=\"http://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center>")
+             self.wfile.write("<center><form name='arquivoupload' method='POST' enctype=\"multipart/form-data\" action='https://"+hostname+":"+baseConfig[1]+"/"+linha+coluna+"uploadarquivo'><br>"+temp[4]+"<br><input type=\"file\" name=\"file\" size=\""+temp[5]+"\"><br><br><button "+bstyle+" onclick=\"javascript:if (confirm('"+baseConfig[8]+"')){document.arquivoupload.submit()};return false;\">Upload</button></form><button "+bstyle+" onclick='javascript:window.document.location=\"https://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center>")
            elif self.path.endswith("executaTarefa"):
              # EVOCACAO DO COMANDO
              result=0
@@ -177,7 +68,7 @@ class Handler(BaseHTTPRequestHandler):
                 else:
                    self.wfile.write("<center><br> "+temp[5]+"<br><br><button "+bstyle+" onclick='javascript:history.go(-1)'>"+baseConfig[6]+"</button></center>")
              if tipo == "1":
-                self.wfile.write("<center><br>"+temp[4]+"<br><br><button "+bstyle+" onclick='javascript:window.document.location=\"http://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center>")   
+                self.wfile.write("<center><br>"+temp[4]+"<br><br><button "+bstyle+" onclick='javascript:window.document.location=\"https://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center>")   
 
 
            else:
@@ -207,7 +98,7 @@ class Handler(BaseHTTPRequestHandler):
                       elif (str(botao[1])=='5'): 
                         acao='uploadprepara'
                          
-                      self.wfile.write("<button "+bstyle+" onclick=\"javascript:if ("+confirm+"){window.document.location='http://"+hostname+":"+baseConfig[1]+"/"+str(x)+str(y)+acao+"'}\">"+botao[0]+"</button>&nbsp;")
+                      self.wfile.write("<button "+bstyle+" onclick=\"javascript:if ("+confirm+"){window.document.location='https://"+hostname+":"+baseConfig[1]+"/"+str(x)+str(y)+acao+"'}\">"+botao[0]+"</button>&nbsp;")
                                             
  
                       
@@ -232,17 +123,17 @@ class Handler(BaseHTTPRequestHandler):
         if self.path.endswith("salvaarquivo"):
            form = cgi.FieldStorage(fp=self.rfile,headers=self.headers,environ={'REQUEST_METHOD':'POST','CONTENT_TYPE':self.headers['Content-Type'],})
            self.send_response(200)
-           #self.send_header('Content-type','text/html')
+           self.send_header('Content-type','text/html; charset=utf-8')
            linha=self.path[1]
            coluna=self.path[2]
            temp = config[int(linha)][int(coluna)]
-           print "Arquivo salvo: "+str(temp[2])
+           print "Fichier sauvegardé : "+str(temp[2])
            f = open(temp[2],"wb") 
            f.write(form.getvalue("texto").replace("\r\n","\n"))
            f.close()
            self.end_headers()
-           self.wfile.write("<html><head></head><body style='color:white;background-color:grey'><center>"+baseConfig[5]+" AdminWeb 1.0</center>") 
-           self.wfile.write("</center><br><br><center>Arquivo Salvo<br><br><button "+bstyle+" onclick='javascript:window.document.location=\"http://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center></body></html>") 
+           self.wfile.write("<html><head></head><body style='color:white;background-color:grey'><center>"+baseConfig[5]+"</center>") 
+           self.wfile.write("</center><br><br><center>Fichier sauvegardé<br><br><button "+bstyle+" onclick='javascript:window.document.location=\"https://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center></body></html>") 
         elif self.path.endswith("uploadarquivo"):
            linha=self.path[1]
            coluna=self.path[2]
@@ -255,8 +146,8 @@ class Handler(BaseHTTPRequestHandler):
            f.close()
            self.send_response(200)
            self.end_headers()
-           self.wfile.write("<html><head></head><body style='color:white;background-color:grey'><center>"+baseConfig[5]+" AdminWeb 1.0</center>")
-           self.wfile.write("</center><br><br><center>"+temp[6]+"<br><br><button "+bstyle+" onclick='javascript:window.document.location=\"http://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center></body></html>")
+           self.wfile.write("<html><head></head><body style='color:white;background-color:grey'><center>"+baseConfig[5]+"</center>")
+           self.wfile.write("</center><br><br><center>"+temp[6]+"<br><br><button "+bstyle+" onclick='javascript:window.document.location=\"https://"+hostname+":"+baseConfig[1]+"/\"'>"+baseConfig[6]+"</button></center></body></html>")
 
       except Exception , e:
           print e
@@ -266,6 +157,7 @@ class Handler(BaseHTTPRequestHandler):
 def main ():
     try:
         server = HTTPServer(('',int(baseConfig[1])), Handler)
+	server.socket = ssl.wrap_socket(server.socket, certfile=baseConfig[9], server_side=True)
         server.serve_forever()
     except Exception , e:
         server.socket.close()
---
+++ config.py
@@ -0,0 +1,119 @@
+# -*- coding: UTF-8 -*-
+#START  CONFIGURATION
+#General Service Configuration - ip (use 0.0.0.0 and keep it generic), port , hostname(do not use,but keep the empty string) , user , password, head of the pages,label of the button that brings the main screen, label of the save button,label of the confirmation, ssl certificate
+baseConfig = ['0.0.0.0','10500','','administrateur','zazovasu','École St-Michel Garicoitz','Page principale','Sauvegarder','Êtes-vous sûr ?','/etc/ssl/routeur.pem']
+
+service = '1'
+commande = '2'
+editeur = '3'
+rapport = '4'
+fichier = '5'
+
+config = [
+
+           [
+            'Système',
+            ['Redémarrer',service,'reboot','1','Le système redémarre'],
+            ['Arrêter',service,'shutdown -h now','1','Arrêt du système en cours.'],
+           ],
+         
+           [
+            'Interfaces réseau',
+            ['Configurer',editeur,'/etc/network/interfaces','0'],
+            ['Réinitialiser',service,['/etc/init.d/networking','restart'],'1','Assurez-vous que tout fonctionne comme prévu…'],
+           ],
+
+           [
+            'Ordinateurs du réseau (dnsmasq)',
+            ['Relancer',service,['/etc/init.d/dnsmasq','restart'],'0','Le serveur de noms et d\'adresses a bien redémarré'],
+            ['Hôtes connus',editeur,'/etc/ethers','0'],
+            ['Inconnus',rapport,"""IFS=$'\n' ; for rapport in $(nmap -T4 -sP 192.168.40.0/24 | grep 192.168.40.[09]*) ; do 
+                                     dns=$(echo $rapport | cut -d" " -f5) ;
+                                     grep -q $dns /etc/hosts || \
+                                         nmap -T4 -sP $dns | \
+                                         grep -v "Starting Nmap 6.00" | grep -v "Host is up" | grep -v "Nmap done" | \
+                                         sed s/"Nmap scan report for "/""/g | sed s/"MAC Address"/"Adresse MAC "/g ;
+                                     done""",'0'],
+            ['Noms d\'hôtes',editeur,'/etc/hosts','0'],
+           ],
+
+           [
+            'Réseau sécurisé (ipsec)',
+            ['Relancer',service,['/etc/init.d/ipsec','restart'],'1','Le reseau sécurisé a bien été relancé ; assurez-vous de réamorcer les connexions des ordinateurs clients.'],
+            ['Paramètres',editeur,'/etc/ipsec.conf','0'],
+            ['Sécurité',editeur,'/etc/ipsec.secrets','0'],
+           ],
+
+           [
+            'Serveur proxy (squid)',
+            ['Arrêter',service,['/etc/init.d/squid34','stop'],'1','Vérifiez les éventuelles erreurs.'],
+            ['Démarrer',service,['/etc/init.d/squid34','start'],'1','Vérifiez les éventuelles erreurs.'],
+            ['Statut',commande,'ps -ef | grep -i squid| grep -vi grep','0','Proxy démarré','Proxy arrêté'],
+#            ['Recharger',service,['/usr/sbin/squid','-k','reconfigure'],'1','Check the log after this operation.'],
+            ['Recharger',commande,'cd /root/proxy/ && ./listes.sh','1','Vérifiez les éventuelles erreurs.'],
+            ['Configurer',editeur,'/etc/squid34/squid.conf','0'],
+            ['Autorisé',editeur,'/root/proxy/autorise','0'],
+            ['Interdit',editeur,'/root/proxy/interdit','0'],
+            ['Horaires',editeur,'/root/proxy/heures','0'],
+            ['Accès',rapport,'tail -500 /var/log/squid/access.log','0'],
+            ['Erreurs',rapport,'cat /var/log/squid/cache.log','0'],
+           ],
+
+#           [
+#            'POUND',
+#            ['Stop','1',['/etc/init.d/pound','stop'],'1','Check the log after this operation.'],
+#            ['Start','1',['/etc/init.d/pound','start'],'1','Check the log after this operation.'],
+#            ['Status','2','ps -ef | grep -i pound| grep -vi grep','0','Pound ON','Pound OFF'],
+#            ['Configuration','3','/etc/pound/pound.cfg','0'],
+#            ['Error Log','4','tail -500 /var/log/syslog','0'],
+#           # ['Upload','5','/home/webadmin/','0','File to upload','40','File Upload OK'],
+#           ],
+
+         ]
+#END - CONFIGURATION
+
+#Per administered service configuration
+#You must add an array named Config that has N child arrays for each set of buttons in the following format:
+#First String with then name of the administered service 
+# N Buttons of that administered service. It can be of the following types (5):
+
+# Type 1 - "Call with screen" format (used in scripts of /etc/init.d to start/stop/reload services):
+ #Label
+ #Button type : 1
+ #Array of command and parameters
+ #1 - Has confirmation prompt 0 - do not has confirmation prompt
+ #Message to present in the end of processing (no matter what happened, this message wil be shown)
+
+#Type 2 - "System with Screen" format (used to execute scripts - like status - and capture the result):
+#DANGER: DO NOT USE THIS BUTTON TO START SCRIPTS THAT WILL LAUNCH PROCESSES IN BACKGROUD(like /etc/init.d scripts) - USE TYPE 1 IN THIS CASE 
+#DANGER: USE THIS TYPE TO LAUNCH SCRIPTS THAT WILL END WITHOUT going BACKGROUND 
+ #Label 
+ #Button type : 2
+ #String with the command (can have pipes)
+ #1 - Has confirmation prompt 0 - do not has confirmation prompt
+ #return menssage when success
+ #return message when fails
+
+#Type 3 - "File Edit" format
+ #Label 
+ #Button type : 3
+ #String with the file to be edited
+ #1 - Has confirmation prompt 0 - do not has confirmation prompt
+ 
+#Type 4 - "Watch result file" format (Used to run commands  that will show results - like a processing with awk  ):
+ #Label 
+ #Button type  : 4
+ #String with the commands that will have the result piped to a known file that will be shown 
+ #1 - Has confirmation prompt 0 - do not has confirmation prompt
+ 
+
+#Type 5 - "File upload" format:
+ #Label 
+ #Button type : 5
+ #Upload directory (like "/opt/test/" dont forget the bars)
+ #1 - Has confirmation prompt 0 - do not has confirmation prompt
+ #Message in the screen that chooses the file 
+ #Size of the label that shows the file name
+ #Menssage in case of success
+
+
